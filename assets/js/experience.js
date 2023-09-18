509dcbf3262d242b4d4ac0a6b28a632aba850e7d
mini-nijgal <dhamininijgal@gmail.com>
2023-09-18 21:20:19 +0530

    Experience changes

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,25 +5,25 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "Assistant Professor",
+    cardImage: "assets/images/experience-page/cu.png",
+    place: "Christ University",
+    time: "(May, 2021 - present)",
+    desp: "<li>I'm currently working as an Assistant professor, holding multiple titles like Batch coordinator (2021-2023), Internship Coordinator, etc.</li>",
   },
   {
-    title: "Student Developer",
+    title: "Teaching Assistant",
     cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+    place: "SRM Institute of Science and Technology",
+    time: "(2018 - 2021)",
+    desp: "<li>I supported the educators by assisting in classroom activities, grading, and guidance. I played a vital role in maintaining a productive learning environment.</li>",
   },
   {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    title: "Assistant Professor",
+    cardImage: "assets/images/experience-page/BharataMataCollege.png",
+    place: "Bharatha Mata College",
+    time: "2016-2018",
+    desp: "<li>As an Assistant Professor, I was responsible for developing and delivering courses, advising students, conducting and participating in academic committees.</li><li>",
   },
 ];
 
